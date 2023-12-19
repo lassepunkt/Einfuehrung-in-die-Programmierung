4f471b405f06c48b10e8afc4a1e223eb2d7aff19
lassepunkt <122087541+lassepunkt@users.noreply.github.com>
2023-12-19 22:34:29 +0100

    Update Kind.java

--- Aufgabenblatt 5/Vergütung im Krankenhaus/Kind.java
+++ Aufgabenblatt 5/Vergütung im Krankenhaus/Kind.java
@@ -1,8 +1,32 @@
-/*
+/**
  * Klasse zur Modellierung eines Kindes
- * 
- * Ist von Ihnen anzupassen
  */
-class Kind {
-    
+class Kind extends Patient {
+    /**
+     * Alter des Patienten
+     */
+    private int alter;
+    /**
+     * Erzeugt ein Kind
+     * @param name Name des Patienten
+     * @param krankheitsklasse Krankheitsklasse des Patienten
+     * @param alter Alter des Patiente
+     */
+    public Kind(String name, int krankheitsklasse, int alter) {
+        super(name, krankheitsklasse);
+        this.alter = alter;
+    }
+    /**
+     * Gibt die Vergütung des Patienten zurück
+     */
+    @Override
+    public double getVerguetung() {
+        return super.getVerguetung()*1.25;
+    }
+    /**
+     * Gibt den Namen, den Typ (Kind) und das Alter des Patienten aus
+     */
+    public void zeigePatient() {
+        System.out.println(super.getName()+" (Kind, "+this.alter+" Jahre)");
+    }
 }
