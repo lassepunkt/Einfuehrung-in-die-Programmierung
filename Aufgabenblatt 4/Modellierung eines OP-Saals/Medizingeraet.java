b6861c73d1665d10dafb34481750d62d35947ef0
lassepunkt <122087541+lassepunkt@users.noreply.github.com>
2023-12-19 21:44:37 +0100

    Create Medizingeraet.java

---
+++ Aufgabenblatt 4/Modellierung eines OP-Saals/Medizingeraet.java
@@ -0,0 +1,21 @@
+/**
+ * Klasse zur Modellierung eines Medizingeraetes
+ */
+public class Medizingeraet {
+    // *** Ergaenzen Sie hier Ihre Implementierung
+    private String name;
+    private String typ;
+    
+    public Medizingeraet(String name, String typ) {
+        this.name = name;
+        this.typ = typ;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getTyp() {
+        return this.typ;
+    }
+}
