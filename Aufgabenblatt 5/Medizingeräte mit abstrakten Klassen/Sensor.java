ba03a9d7cde5f9c0d8c6d63be64e402a01c3ec47
lassepunkt <122087541+lassepunkt@users.noreply.github.com>
2023-12-23 15:19:39 +0100

    Create Sensor.java

---
+++ Aufgabenblatt 5/Medizingeräte mit abstrakten Klassen/Sensor.java
@@ -0,0 +1,25 @@
+public class Sensor extends Medizingeraet {
+    private int messwert;
+    
+    public Sensor(String hersteller, int alarmSchwellwert) {
+        super(hersteller, alarmSchwellwert);
+    }
+    
+    private void messen() {
+        this.messwert = rand.nextInt(100);
+    }
+    
+    @Override
+    public void ueberpruefeAlarm() {
+        if (!super.istEingeschaltet) {
+            System.out.println("Geraet ist ausgeschaltet!");
+        } else {
+            this.messen();
+            if (this.messwert > super.alarmSchwellwert) {
+                System.out.println("Alarm! Messwer uebersteigt Schwellwert.");
+            } else {
+                System.out.println("Messwert ist in Ordnung.");
+            }
+        }
+    }
+}
