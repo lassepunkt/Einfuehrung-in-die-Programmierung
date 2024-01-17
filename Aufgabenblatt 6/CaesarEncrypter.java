f67287ab0ebdf9bc8f0beaae31d70ac19abe6749
lassepunkt <122087541+lassepunkt@users.noreply.github.com>
2024-01-17 20:56:08 +0100

    Update CaesarEncrypter.java

--- Aufgabenblatt 6/CaesarEncrypter.java
+++ Aufgabenblatt 6/CaesarEncrypter.java
@@ -1,4 +1,4 @@
-public class CaesarEncrypter implements Encrypter {
+public class CaesarEncrypter implements Encrypter{
     private int schluessel;
     private String alphabet = "abcdefghijklmnopqrstuvwxyz .,!?";
     
@@ -9,23 +9,15 @@ public class CaesarEncrypter implements Encrypter {
     public String encrypt(String message) {
         String encryptedMessage = "";
         for (int i = 0; i < message.length(); i++) {
-            for (int a = 0; a < alphabet.length(); a++) {
-                if (message.charAt(i) == alphabet.charAt(a)) {
-                    encryptedMessage += alphabet.charAt((a+this.schluessel)%alphabet.length());
-                }
-            }
+            encryptedMessage += alphabet.charAt((alphabet.indexOf(message.charAt(i))+this.schluessel)%alphabet.length());
         }
         return encryptedMessage;
     }
-    
+
     public String decrypt(String message) {
         String decryptedMessage = "";
         for (int i = 0; i < message.length(); i++) {
-            for (int a = 0; a < alphabet.length(); a++) {
-                if (message.charAt(i) == alphabet.charAt(a)) {
-                    decryptedMessage += alphabet.charAt((a+(alphabet.length()-this.schluessel))%alphabet.length());
-                }
-            }
+            decryptedMessage += alphabet.charAt((alphabet.indexOf(message.charAt(i))+(alphabet.length()-this.schluessel))%alphabet.length());
         }
         return decryptedMessage;
     }
